747ef98b94bc855374b658e18be1f8c5e243cbf2
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-06-18 14:36:59 +0000

    add

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,23 @@
 class Admin::CustomersController < ApplicationController
   def index
-    
+    @admin_customer = Customer.page(params[:page])
   end
-  
+
   def show
-  end 
-  
+    @admin_customer = Customer(params[:id])
+  end
+
   def edit
-  end 
-  
+  end
+
   def update
-  end 
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
+
+  end
 
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -7,7 +7,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
    protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephon_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
 
 
