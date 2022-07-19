e5ba583668edcbb338d18a39802e857b57aa6ca1
nakamuramano <104703112+nakamuramano@users.noreply.github.com>
2022-07-19 18:05:00 +0000

    add

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
-  
-  
+
+  def show
+
+  end
+
+    private
+
+  def order_params
+    params.require(:order_detail).permit(:amount, :tax_included_price)
+  end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,15 @@
 class Admin::OrdersController < ApplicationController
-  
+
   def show
-    @order_detail = OrderDetail.find(params[:id])
-    @order = current_user.order_id
+    @order = Order.find(params[:id])
+    @orders = @order.cuatomer
     @total = 0
   end
-  
+
   private
-  
+
   def order_params
-    params.require(:or_detail).permit(:amount, :tax_included_price)
-  end 
+    params.require(:order).permit(:amount, :tax_included_price)
+  end
 
 end
